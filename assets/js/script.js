7157cd8a9208b714fe1e31d505f79cd6d261d179
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-01-30 16:05:40 -0700

    build out HTML elements and add JS event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,40 @@
     // descriptions
     // hours
 
+var saveBtn = document.querySelector(".saveBtn");
+
+
+// declare global variables
+
+
+
+// declare functions
+
+function setCurrentDate() {
+    var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm a");
+    $("#current-date").text(currentDate);
+
+}
+
+
+
+setCurrentDate();
+
+
+
+
+// add event listeners
+
+$(".saveBtn").on("click", function() {
+    
+    console.log("saved!");
+
+
+
+});
 
 
 
 
 
 
-
-
-
-
-
-// event listeners
-    // save buttons
